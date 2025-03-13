a949f55e58c0b1f28c2e6b054d1658db275218cb
ShahnamFeyzian <shahnamecfman@gmail.com>
2025-03-13 18:55:57 +0330

    init: Add base of web api module

---
+++ CA3/MioBook/WebApi/src/main/java/webapi/MioBookApplication.java
@@ -0,0 +1,12 @@
+package webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MioBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MioBookApplication.class, args);
+	}
+}
