204721afcfb75df631ab76e31180700aaa9b3cfa
ShahnamFeyzian <shahnamecfman@gmail.com>
2025-03-15 18:40:14 +0330

    feat: Add AuthenticationException in presentation layer

--- CA3/MioBook/WebApi/src/main/java/webapi/services/AuthenticationService.java
+++ CA3/MioBook/WebApi/src/main/java/webapi/services/AuthenticationService.java
@@ -2,10 +2,21 @@ package webapi.services;
 
 import domain.entities.User;
 import org.springframework.stereotype.Service;
+import webapi.exceptions.AuthenticationException;
 
 @Service
 public class AuthenticationService {
-    private User user;
+    private User user = null;
+
+    public void validateAnyOneLoggedIn() {
+        if (user == null)
+            throw AuthenticationException.noOneLoggedIn();
+    }
+
+    public void validateNoOneLoggedIn() {
+        if (user != null)
+            throw AuthenticationException.someOneLoggedIn();
+    }
 
     public User.Role getUserRole() {
         return user.getRole();
@@ -16,5 +27,9 @@ public class AuthenticationService {
     
     public void setLoggedInUser(User user) {
         this.user = user;
+    }
+
+    public void unSetLoggedInUser() {
+        this.user = null;
     }
 }
