e17e41224a138fd261186d5e1ad3b353fc8eeb04
Shahriar-0 <attarshahriar@gmail.com>
2025-03-18 19:23:49 +0330

    feat: Enhance DataInitializer to load authors, books, and reviews from external API

--- CA3/MioBook/WebApi/src/main/java/webapi/configuration/DataInitializer.java
+++ CA3/MioBook/WebApi/src/main/java/webapi/configuration/DataInitializer.java
@@ -1,11 +1,15 @@
 package webapi.configuration;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import application.result.Result;
 import application.usecase.UseCaseType;
+import application.usecase.admin.AddAuthorUseCase;
+import application.usecase.admin.AddBookUseCase;
+import application.usecase.customer.AddReviewUseCase;
 import application.usecase.user.AddUserUseCase;
-
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import domain.entities.Book;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
@@ -20,11 +24,17 @@ public class DataInitializer implements ApplicationRunner {
 
 	private static final String API_URL = "http://194.60.230.196:8000";
 	private static final String USERS_API_URL = API_URL + "/users";
+	private static final String AUTHORS_API_URL = API_URL + "/authors";
+	private static final String BOOKS_API_URL = API_URL + "/books";
+	private static final String REVIEWS_API_URL = API_URL + "/reviews";
 	private final UseCaseService useCaseService;
 
 	@Override
 	public void run(ApplicationArguments args) {
 		loadUsers();
+		loadAuthors();
+		loadBooks();
+		loadReviews();
 	}
 
 	private void loadUsers() {
@@ -36,12 +46,70 @@ public class DataInitializer implements ApplicationRunner {
 			ObjectMapper objectMapper = new ObjectMapper();
 			List<AddUserUseCase.AddUserData> parsedUserData = objectMapper.readValue(jsonResponse, new TypeReference<>() {});
 
-			for (AddUserUseCase.AddUserData userData : parsedUserData) {
-				addUser.perform(userData);
-			}
+			parsedUserData.forEach(addUser::perform);
 		}
 		catch (Exception e) {
 			System.err.println("Error loading users: " + e.getMessage());
+		}
+	}
+
+	private void loadAuthors() {
+		AddAuthorUseCase addAuthor = (AddAuthorUseCase) useCaseService.getUseCase(UseCaseType.ADD_AUTHOR);
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			String jsonResponse = restTemplate.getForObject(AUTHORS_API_URL, String.class);
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			List<AddAuthorUseCase.AddAuthorData> parsedAuthorData = objectMapper.readValue(jsonResponse, new TypeReference<>() {});
+
+			parsedAuthorData.forEach(addAuthor::perform);
+		}
+		catch (Exception e) {
+			System.err.println("Error loading authors: " + e.getMessage());
+		}
+	}
+
+	private void loadBooks() {
+		AddBookUseCase addBook = (AddBookUseCase) useCaseService.getUseCase(UseCaseType.ADD_BOOK);
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			String jsonResponse = restTemplate.getForObject(BOOKS_API_URL, String.class);
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			List<AddBookUseCase.AddBookData> parsedBookData = objectMapper.readValue(jsonResponse, new TypeReference<>() {});
+
+			parsedBookData.forEach(addBook::perform);
+		}
+		catch (Exception e) {
+			System.err.println("Error loading books: " + e.getMessage());
+		}
+	}
+
+	private void loadReviews() {
+		AddReviewUseCase addReview = (AddReviewUseCase) useCaseService.getUseCase(UseCaseType.ADD_REVIEW);
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			String jsonResponse = restTemplate.getForObject(REVIEWS_API_URL, String.class);
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			JsonNode rootNode = objectMapper.readTree(jsonResponse);
+
+			for (JsonNode node : rootNode) {
+				String username = node.get("username").asText();
+				String title = node.get("title").asText();
+				int rating = node.get("rate").asInt();
+				String comment = node.get("comment").asText();
+
+				AddReviewUseCase.AddReviewData addReviewData = new AddReviewUseCase.AddReviewData(title, rating, comment);
+
+				Result<Book> result = addReview.perform(addReviewData, username);
+				if (result.isFailure()) {
+					System.err.println("Failed to add review for book: " + title);
+				}
+			}
+		}
+		catch (Exception e) {
+			System.err.println("Error loading reviews: " + e.getMessage());
 		}
 	}
 }
