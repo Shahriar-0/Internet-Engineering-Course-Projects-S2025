e49a8f6eecca8adece937ac7c33c660d0bd2a20a
ShahnamFeyzian <shahnamecfman@gmail.com>
2025-03-29 02:52:42 +0330

    init: Create base structure of project and add some necessary packages

--- CA4/MioBook/webclient/src/App.js
+++ CA4/MioBook/webclient/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./layout/header/Header";
+import Footer from "./layout/footer/Footer";
+import SignIn from "./layout/main/sign-in/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ CA4/MioBook/webclient/src/layout/header/Header.jsx
@@ -0,0 +1,27 @@
+﻿
+const Header = () => {
+    return (
+      <header className="shadow py-3 px-5 mb-5">
+          <div className="container p-0 m-0 mx-auto d-flex flex-wrap align-items-center justify-content-between">
+              <a href="../homepage/homepage.html"><img src="../../assets/../assets/icons/logo-icon.svg" alt="MioBook"/></a>
+              <div className="d-none d-sm-flex bg-gray w-50 w-xl-75 p-2 rounded-3">
+                  <label htmlFor="filter-type" className="visually-hidden">Filter Type</label>
+                  <select id="filter-type" name="filterType"
+                          className="text-secondary me-2 px-3 border-0 bg-transparent outline-0 pointer">
+                      <option className="text-secondary bg-gray">Name</option>
+                      <option className="text-secondary bg-gray">Author</option>
+                      <option className="text-secondary bg-gray">Genre</option>
+                  </select>
+                  <label htmlFor="filter-value" className="visually-hidden">Filter Value</label>
+                  <input id="filter-value" name="filterValue" type="search"
+                         className="flex-grow-1 border-start border-secondary ps-3 border-0 bg-transparent outline-0"
+                         placeholder="Search"/>
+              </div>
+              <button className="d-sm-none btn border-0 fw-bolder bg-gray"><i className="bi bi-search"></i></button>
+              <button className="btn border-1 fw-bold green-btn">Buy now</button>
+          </div>
+      </header>
+    );
+}
+
+export default Header
