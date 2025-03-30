c4b1271b60e480de57328aa65bd49732fdc73d03
ShahnamFeyzian <shahnamecfman@gmail.com>
2025-03-30 05:05:52 +0330

    feat: Add AuthenticationService

---
+++ CA4/MioBook/webclient/src/services/AuthenticationService.js
@@ -0,0 +1,34 @@
+﻿let currentUser = null;
+
+const isAnyUserLoggedIn = () => currentUser !== null;
+
+const isUserLoggedIn = (username) => {
+    if (!isAnyUserLoggedIn())
+        return false;
+
+    return currentUser.username === username;
+}
+
+const getCurrentUser = () => structuredClone(currentUser);
+
+const login = (username, password) => {
+    console.log("Logging in...");
+    console.log("username: " + username + " password: " + password);
+    currentUser = {username: username, password: password};
+};
+
+const logout = () => {
+    console.log("Logging out...");
+    currentUser = null;
+};
+
+const AuthenticationService = Object.freeze({
+    isAnyUserLoggedIn,
+    isUserLoggedIn,
+    getCurrentUser,
+    login,
+    logout,
+});
+
+
+export default AuthenticationService;
