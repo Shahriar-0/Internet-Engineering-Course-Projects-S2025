1d4436d83556ad2a9222f3b9c36ba71294cb839b
ShahnamFeyzian <shahnamecfman@gmail.com>
2025-04-13 10:34:17 +0330

    feat: Add toast to project

--- CA4/MioBook/webclient/src/App.jsx
+++ CA4/MioBook/webclient/src/App.jsx
@@ -5,20 +5,25 @@ import SignIn from "layout/main/sign-in/SignIn";
 import SignUp from "layout/main/sign-up/SignUp";
 
 import UrlService from "services/UrlService";
+import {ToastContainer} from "react-toastify";
 
 function App() {
     const urls = UrlService.urls;
 
     return (
+        <>
         <BrowserRouter>
             <Header/>
             <Routes>
                 <Route path={urls.signIn} element={<SignIn />} />
                 <Route path={urls.signUp} element={<SignUp />}/>
                 <Route path={urls.home} element={<div>Home</div>} />
+                <Route path={urls.unexpectedError} element={<div>Unexpected Error</div>} />
             </Routes>
             <Footer />
         </BrowserRouter>
+        <ToastContainer />
+        </>
     );
 }
 
