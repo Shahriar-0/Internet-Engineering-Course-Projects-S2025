70d55bcc5bfb868a6b6a1d5aa3abac0aff695a6d
ShahnamFeyzian <shahnamecfman@gmail.com>
2025-04-13 19:05:19 +0330

    feat: Add Book and BookConatainer to common components

---
+++ CA4/MioBook/webclient/src/common/book/BookCard.jsx
@@ -0,0 +1,21 @@
+﻿import bookCardImage from "assets/images/books/book-card-image.svg";
+import stars from "assets/icons/stars.svg";
+
+const BookCard = ({bookTitle, authorName, price, rating, bookImage = bookCardImage}) => {
+    return (
+        <div className="card text-center shadow rounded-4 bg-khaki">
+            <img className="card-img-top rounded-top-4" src={bookImage} alt="book-image"/>
+            <div className="card-body">
+                <p className="card-title fw-bold">{bookTitle}</p>
+                <p className="card-text">{authorName}</p>
+                <div className="d-flex justify-content-between mb-2">
+                    <img src={stars} alt="stars-rate"/>
+                    <span>${price}</span>
+                </div>
+                <button className="btn border-1 fw-bold green-btn w-100">Add to Cart</button>
+            </div>
+        </div>
+    );
+}
+
+export default BookCard;
---
+++ CA4/MioBook/webclient/src/common/book/BookCardContainer.jsx
@@ -0,0 +1,15 @@
+﻿import BookCard from "./BookCard";
+
+const BookCardContainer = ({bookList}) => {
+    return (
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
+            {bookList.map((book, index) => (
+                <div className="mb-2" key={index}>
+                    <BookCard key={index}  {...book}/>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default BookCardContainer;
