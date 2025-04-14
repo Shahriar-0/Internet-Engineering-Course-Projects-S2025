66d70fd49ebcddbd577ff59a4ad37f1c83965eba
Shahriar-0 <attarshahriar@gmail.com>
2025-04-15 01:29:00 +0330

    feat: Implement BookCard and BookCardContainer components with API integration for fetching books

--- CA4/MioBook/webclient/src/common/book/BookCard.jsx
+++ CA4/MioBook/webclient/src/common/book/BookCard.jsx
@@ -1,15 +1,15 @@
-﻿import bookCardImage from "assets/images/books/book-card-image.svg";
+﻿import defaultBookCardImage from "assets/images/books/book-card-image.svg";
 import stars from "assets/icons/stars.svg";
 
-const BookCard = ({bookTitle, authorName, price, rating, bookImage = bookCardImage}) => {
+const BookCard = ({ title, author, price, rating, bookImage = defaultBookCardImage }) => {
     return (
         <div className="card text-center shadow rounded-4 bg-khaki">
-            <img className="card-img-top rounded-top-4" src={bookImage} alt="book-image"/>
+            <img className="card-img-top rounded-top-4" src={bookImage} alt="book-image" />
             <div className="card-body">
-                <p className="card-title fw-bold">{bookTitle}</p>
-                <p className="card-text">{authorName}</p>
+                <p className="card-title fw-bold">{title}</p>
+                <p className="card-text">{author}</p>
                 <div className="d-flex justify-content-between mb-2">
-                    <img src={stars} alt="stars-rate"/>
+                    <img src={stars} alt="stars-rate" />
                     <span>${price}</span>
                 </div>
                 <button className="btn border-1 fw-bold green-btn w-100">Add to Cart</button>
--- CA4/MioBook/webclient/src/common/book/BookCardContainer.jsx
+++ CA4/MioBook/webclient/src/common/book/BookCardContainer.jsx
@@ -1,13 +1,53 @@
 ﻿import BookCard from "./BookCard";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import ApiService from "services/ApiService";
+import UrlService from "services/UrlService";
+import { transformBook, getBookCover } from "./BookCardLogic";
 
-const BookCardContainer = ({bookList}) => {
+const BookCardContainer = ({ bookList = null, url }) => {
+
+    const [books, setBooks] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(null);
+    // TODO: add loading
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (bookList == null) {
+            const fetchBooks = async () => {
+                const { body, error } = await ApiService.getBooks(url);
+                console.log(body);
+                if (error) {
+                    setErrorMessage(error);
+                    navigate(UrlService.urls.unexpectedError);
+                }
+                else if (body && body.status === ApiService.statusCode.OK) {
+                    const transformedBooks = body.data.list.map((book) => transformBook(book));
+                    setBooks(transformedBooks);
+                }
+            }
+
+            fetchBooks();
+        }
+        else
+            setBooks(bookList);
+    }, [bookList, navigate, url]);
+
     return (
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
-            {bookList.map((book, index) => (
-                <div className="mb-2" key={index}>
-                    <BookCard key={index}  {...book}/>
+            {errorMessage && (
+                <div className="alert alert-danger" role="alert">
+                    {errorMessage}
                 </div>
-            ))}
+            )}
+            {
+                books.map((book, index) => (
+                    <div className="mb-2" key={index}>
+                        <BookCard key={index}  {...book} />
+                    </div>
+                ))
+            }
         </div>
     );
 }
