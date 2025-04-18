d2a6f9ad2b1a818db8257dc93319d74937b05360
ShahnamFeyzian <shahnamecfman@gmail.com>
2025-04-18 14:35:58 +0330

    feat: Add Accordion to library

---
+++ CA4/MioBook/webclient/src/library/accordion/Accordion.jsx
@@ -0,0 +1,39 @@
+﻿import React, {useState} from "react";
+import AccordionItem from "./AccordionItem";
+
+const Accordion = ({children}) => {
+
+    const [openItemIndex, setOpenItemIndex] = useState(-1);
+
+    const onToggle = (index) => {
+        if (index === openItemIndex)
+            setOpenItemIndex(-1);
+        else
+            setOpenItemIndex(index);
+    }
+
+    const annotateChildren = () => {
+        let currentIndex = 0;
+        return (
+            <>
+                {React.Children.map(children, child => {
+                    if (React.isValidElement(child) && child.type === AccordionItem) { // Remove all children that are not AccordionItem
+                        return React.cloneElement(child, {
+                            isOpen: currentIndex === openItemIndex, // Pass isOpen to AccordionItem
+                            index: currentIndex++, // Give each AccordionItem a unique index that start from 0
+                            onToggle: onToggle // Pass onToggle to AccordionItem
+                        });
+                    }
+                })}
+            </>
+        );
+    }
+
+    return (
+        <div className="d-flex flex-column">
+            {annotateChildren()}
+        </div>
+    )
+}
+
+export default Accordion;
---
+++ CA4/MioBook/webclient/src/library/accordion/AccordionItem.jsx
@@ -0,0 +1,16 @@
+﻿import OpenIcon from "./assets/open-icon.svg";
+import CloseIcon from "./assets/close-icon.svg";
+
+const AccordionItem = ({ children, isOpen, index, onToggle, headerText, headerTextClassName, headerClassName }) => {
+    return (
+        <div>
+            <div className={`bg-gray d-flex justify-content-between align-items-center p-2 ${headerClassName}`}>
+                <p className={`mb-0 ${headerTextClassName}`}>{headerText}</p>
+                <img  className="pointer" onClick={() => onToggle(index)} src={isOpen ? OpenIcon : CloseIcon} alt="open-icon"/>
+            </div>
+            { isOpen ? children : <></>}
+        </div>
+    );
+}
+
+export default AccordionItem;
