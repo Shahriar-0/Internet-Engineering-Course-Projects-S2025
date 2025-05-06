8ea3694c5fe826fa6c2e207978555cce126a3e6e
ShahnamFeyzian <shahnamecfman@gmail.com>
2025-05-06 17:05:09 +0330

    test: Add test scenarios for AuthorController

--- CA5/MioBook/server/WebApi/src/test/java/webapi/fixture/AuthorFixtureUtil.java
+++ CA5/MioBook/server/WebApi/src/test/java/webapi/fixture/AuthorFixtureUtil.java
@@ -1,9 +1,13 @@
 package webapi.fixture;
 
+import application.usecase.admin.author.AddAuthor;
 import domain.entities.author.Author;
 import domain.entities.user.Admin;
+import webapi.views.author.AuthorView;
 
 import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class AuthorFixtureUtil {
 
@@ -25,6 +29,27 @@ public class AuthorFixtureUtil {
         return author;
     }
 
+    public static AddAuthor.AddAuthorData addAuthorData(int index) {
+        AddAuthor.AddAuthorData data = new AddAuthor.AddAuthorData();
+        data.setName(name(index));
+        data.setPenName(penName(index));
+        data.setNationality(NATIONALITY);
+        data.setBorn(born(index).toString());
+        data.setDied(null);
+        return data;
+    }
+
+    public static AuthorView view(int index) {
+        AuthorView view = new AuthorView();
+        view.setName(name(index));
+        view.setPenName(penName(index));
+        view.setNationality(NATIONALITY);
+        view.setBorn(born(index));
+        view.setDied(null);
+        view.setBookCount(0);
+        return view;
+    }
+
     public static String name(int index) {
         return "AuthorName_" + index;
     }
@@ -35,5 +60,13 @@ public class AuthorFixtureUtil {
 
     public static LocalDate born(int index) {
         return LocalDate.of(1990, 1, 1).plusYears(index);
+    }
+
+    public static void assertion(Author a1, Author a2) {
+        assertThat(a1.getName()).isEqualTo(a2.getName());
+        assertThat(a1.getPenName()).isEqualTo(a2.getPenName());
+        assertThat(a1.getNationality()).isEqualTo(a2.getNationality());
+        assertThat(a1.getBorn()).isEqualTo(a2.getBorn());
+        assertThat(a1.getDied()).isEqualTo(a2.getDied());
     }
 }
