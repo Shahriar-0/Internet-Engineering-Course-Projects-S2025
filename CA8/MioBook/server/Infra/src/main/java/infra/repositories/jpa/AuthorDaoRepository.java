0faeb7e7742442ac7d8c4818979df3f9448be3a8
Shahriar-0 <attarshahriar@gmail.com>
2025-05-28 05:10:02 +0330

    fix: Enhance book retrieval by allowing partial match and return empty list when nothing matches

--- CA8/MioBook/server/Infra/src/main/java/infra/repositories/jpa/AuthorDaoRepository.java
+++ CA8/MioBook/server/Infra/src/main/java/infra/repositories/jpa/AuthorDaoRepository.java
@@ -1,12 +1,17 @@
 package infra.repositories.jpa;
 
 import infra.daos.AuthorDao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorDaoRepository extends JpaRepository<AuthorDao, Long> {
-    Optional<AuthorDao> findByName(String name);
+	Optional<AuthorDao> findByName(String name);
+
+	@Query("SELECT a FROM AuthorDao a WHERE LOWER(a.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+	List<AuthorDao> searchByName(@Param("name") String name);
 }
